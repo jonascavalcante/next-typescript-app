8e78a6b7ac49c5d95ec1a37c23644034a108818a
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-04-15 18:06:42 -0300

    add Blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,49 @@
+import Head from "../../components/Head";
+import Header from "../../components/Header";
+
+import { Post } from "../../types/Post";
+
+type Props = {
+  title: string;
+  posts: Post[];
+}
+
+const Blog = ({ title, posts }: Props) => {
+  return (
+    <div>
+      <Head title={title} />
+
+      <Header />
+
+      <main>
+        <h1>{title}</h1>
+        <section>
+          {posts?.map(({ id, title, body }) => (
+            <div key={id}>
+              <h3>{title}</h3>
+              <p>{body}</p>
+              <hr />
+            </div>
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+
+  let res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const posts: Post[] = await res.json();
+
+  return {
+    props: {
+      title: 'Blog',
+      posts
+    },
+    revalidate: 60
+    //tempo em segundos em que os dados estarão em cache até a proxima requisição
+  }
+}
+
+export default Blog;
